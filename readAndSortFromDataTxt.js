7c662f6b308693bac1ed2d16b412a9f8b825daf2
Peshraw <peshrawsarwar@gmail.com>
2021-09-28 13:56:01 +0300

    soting / reading numbers from another file sorting them and writing them in a seperate file

---
+++ readAndSortFromDataTxt.js
@@ -0,0 +1,27 @@
+// importing the file system to read files.
+const fs = require('fs')
+numbers = [];
+fs.readFile('data.txt', 'UTF-8', (err, data) => {
+    if (err) throw err;
+    //putting the numbers inside an array
+    numbers = data.split(',').map(num => parseInt(num, 10))
+    let temp = 0;
+    let n = numbers.length;
+
+    //sorting
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (numbers[j] > numbers[j + 1]) {
+                temp = numbers[j];
+                numbers[j] = numbers[j + 1]
+                numbers[j + 1] = temp;
+            }
+        }
+    }
+
+    console.log("numbers list After sorting: ", numbers)
+        //writing the numbers to a file.
+    fs.writeFile('newDatas.txt', JSON.stringify(numbers), err => {
+        console.log(err);
+    });
+})
--- task.js
+++ task.js
@@ -1,41 +1,37 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
+let numbers = [5, 8, 0, 1, 9, 11, 15, 16];
 
-console.log("Original numbers list: ", numbers)
+console.log("Original numbers list: ", numbers);
+
+// putting the numbers inside antoher array to keep the original one as it's.
+let newNumbers = [...numbers];
 
 let temp = 0;
-let n = numbers.length;
+let n = newNumbers.length; // the length of the array
 
 for (let i = 0; i < n - 1; i++) {
     for (let j = 0; j < n - i - 1; j++) {
-        if (numbers[j] > numbers[j + 1]) {
-            temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
+        //if the index before the other index is greater than then :
+        if (newNumbers[j] > newNumbers[j + 1]) {
+            temp = newNumbers[j]; //insert the greater index to temp veriable
+
+            newNumbers[j] = newNumbers[j + 1]; //insert the smaller one to the index of the greater one
+
+            newNumbers[j + 1] = temp; //insert the greater one to the index of the smaller one
         }
     }
 }
 
-console.log("numbers list After sorting: ", numbers)
-
+console.log("numbers list After sorting: ", newNumbers);
 
 for (let i = 0; i < n - 1; i++) {
     for (let j = 0; j < n - i - 1; j++) {
-
-        if (numbers[j] < numbers[j + 1]) {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
+        //if the index before the other index is less than then :
+        if (newNumbers[j] < newNumbers[j + 1]) {
+            let temp = newNumbers[j];
+            newNumbers[j] = newNumbers[j + 1];
+            newNumbers[j + 1] = temp;
         }
     }
 }
 
-console.log("numbers list After Desc sorting: ", numbers)
+console.log("numbers list After Desc sorting: ", newNumbers);
